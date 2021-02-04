e1a8f45a35e80dd3e3bade0ad2c8cb35021e0ff3
Wasef <alex.wasef@gmail.com>
2021-02-04 13:43:20 +0100

    Ajoutez des fichiers projet.

---
+++ Web/WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Web.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Hello World</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="IbName">
+            <asp:Label ID="lbName" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+        </div>
+        <asp:ListBox ID="Location" runat="server">
+            <asp:ListItem>Mumbai</asp:ListItem>
+            <asp:ListItem>Bangalore</asp:ListItem>
+            <asp:ListItem>Hydrebad</asp:ListItem>
+        </asp:ListBox>
+        <p>
+            <asp:RadioButton ID="Male" runat="server" Text="Male" />
+        </p>
+        <asp:RadioButton ID="Female" runat="server" Text="Female" />
+        <p>
+            <asp:CheckBox ID="CheckC" runat="server" Text="C#" />
+        </p>
+        <asp:CheckBox ID="CheckAsp" runat="server" Text="Asp.Net" />
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Envoyer" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Web/WebForm1.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace Web
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Contrôle form1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Contrôle lbName.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbName;
+
+        /// <summary>
+        /// Contrôle txtName.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+
+        /// <summary>
+        /// Contrôle Location.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox Location;
+
+        /// <summary>
+        /// Contrôle Male.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton Male;
+
+        /// <summary>
+        /// Contrôle Female.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButton Female;
+
+        /// <summary>
+        /// Contrôle CheckC.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckC;
+
+        /// <summary>
+        /// Contrôle CheckAsp.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox CheckAsp;
+
+        /// <summary>
+        /// Contrôle Button1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}
